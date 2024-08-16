e65848ca5d6113ad189051cb8743cd34da2e7957
Joao1berto <161041026+Joao1berto@users.noreply.github.com>
2024-08-16 10:41:06 -0300

    Add files via upload

---
+++ Flask/PYTHONFLASKG1/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/aula')
+@app.route('/aula/<nome>')
+@app.route('/aula/<nome>/<curso>/<int:ano>')
+def aula(nome = 'Maria', curso = 'Informáica', ano = 1):
+    dados = {'nome': nome, 'curso': curso, 'ano': ano}
+    return render_template('aula.html', dados_html=dados)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/dados', methods=['POST'])
+def dados():
+    dados = request.form
+    return render_template('dados.html', dados = dados)
+
+if __name__ == '__main__':
+    app.run()
